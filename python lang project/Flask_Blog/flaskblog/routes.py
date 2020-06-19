5f7b5b675356b353ec9dafa07eecd6772c2805c9
joelly <j.ekomalet@gmail.com>
2020-06-19 19:23:11 -0400

    wrote down notes on user authentication

--- python lang project/Flask_Blog/flaskblog/routes.py
+++ python lang project/Flask_Blog/flaskblog/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog.models import User, Post
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog import app, db, bcrypt
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 my_posts = [
     {
@@ -57,8 +57,18 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
     
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')

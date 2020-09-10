36ad63e6ff4e12dbe0147d499a2f1f529c7989a1
Joelly Malet <j.ekomalet@gmail.com>
2020-09-10 08:58:40 -0400

    password reset

--- python lang project/Flask_Blog/flaskblog/__init__.py
+++ python lang project/Flask_Blog/flaskblog/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -15,6 +16,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = "info"
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'liljearsy@gmail.com'
+app.config['MAIL_PASSWORD'] = '47h_m@luaA24'
+mail = Mail()
 
 # our routes import the flask app
 # so we cant import routes in this file
